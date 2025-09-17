811d1cf7d46fdf9ae59c773eaeb63bd428765eb7
Eric Siegel <NTICompass@gmail.com>
2025-09-17 09:32:03 -0400

    save keyboxes to xml files

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from downloaders.integritybox import IntegrityBox
 from downloaders.trickyaddon import TrickyAddon
 from downloaders.yurikey import YuriKey
+import os
 
 
 if __name__ == '__main__':
+    path = 'keyboxes'
+    if not os.path.exists(path):
+        os.mkdir(path)
+
     for dl in (IntegrityBox(), TrickyAddon(), YuriKey()):
-        print(dl.get_keybox())
+        keybox = dl.get_keybox()
+
+        with open('{}/{}.xml'.format(path, type(dl).__name__), 'w', encoding='utf-8') as f:
+            f.write(keybox)
