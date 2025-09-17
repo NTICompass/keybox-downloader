a54f5ca75a1122cb22f0d3b2c1fb86372be075f1
Eric Siegel <NTICompass@gmail.com>
2025-09-17 18:31:52 -0400

    check the validity of the keybox thanks to https://github.com/Ante0/YetAnotherKeyboxChecker

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from downloaders.integritybox import IntegrityBox
 from downloaders.trickyaddon import TrickyAddon
 from downloaders.tsupport import TSupport
 from downloaders.yurikey import YuriKey
-import os
 from tqdm import tqdm
+from verify.googlecheck import GoogleChecker
+import os
+import xml.etree.ElementTree as ET
 
 
 def write_xml(file: str, data: str):
@@ -13,14 +15,24 @@ def write_xml(file: str, data: str):
 
 if __name__ == '__main__':
     path = 'keyboxes'
+
     if not os.path.exists(path):
-        os.mkdir(path)
+        os.makedirs('{}/{}'.format(path, 'valid'))
+        os.makedirs('{}/{}'.format(path, 'revoked'))
+
+    checker = GoogleChecker()
 
     for dl in tqdm((IntegrityBox(), TrickyAddon(), TSupport(), YuriKey())):
         keybox = dl.get_keybox()
 
         if isinstance(keybox, str):
-            write_xml('{}/{}.xml'.format(path, type(dl).__name__), keybox)
+            valid_keybox = checker.is_keybox_revoked(ET.fromstring(keybox))
+            save_path = '{}/{}'.format(path, 'valid' if valid_keybox else 'revoked')
+
+            write_xml('{}/{}.xml'.format(save_path, type(dl).__name__), keybox)
         else:
             for (idx, keybox_file) in enumerate(keybox):
-                write_xml('{}/{}_{}.xml'.format(path, type(dl).__name__, idx), keybox_file)
+                valid_keybox = checker.is_keybox_revoked(ET.fromstring(keybox_file))
+                save_path = '{}/{}'.format(path, 'valid' if valid_keybox else 'revoked')
+
+                write_xml('{}/{}_{}.xml'.format(save_path, type(dl).__name__, idx), keybox_file)
---
+++ verify/googlecheck.py
@@ -0,0 +1,43 @@
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from time import time
+from typing import Optional
+from xml.etree.ElementTree import Element
+import requests
+
+
+def get_cert_info(cert_pem: Element) -> tuple[Optional[str], Optional[str]]:
+    try:
+        cert = x509.load_pem_x509_certificate(cert_pem.text.encode(), default_backend())
+    except ValueError:
+        return None, None
+
+    hex_serial = format(cert.serial_number, 'x').lower().lstrip('0')
+    issuer_serial = {attr.value.lower() for attr in cert.issuer if attr.oid == x509.NameOID.SERIAL_NUMBER}
+
+    return hex_serial, str(issuer_serial.pop()) if len(issuer_serial) > 0 else None
+
+"""
+Thanks to https://github.com/Ante0/YetAnotherKeyboxChecker
+"""
+class GoogleChecker:
+    URL = 'https://android.googleapis.com/attestation/status?' + str(int(time()))
+
+    def __init__(self):
+        keybox_status = requests.get(self.URL, headers={
+            'Accept-Encoding': 'br, gzip',
+            'Cache-Control': 'no-cache',
+        }).json()
+
+        self.revoked: set[str] = {key for (key, status) in keybox_status['entries'].items() if status['status'] == 'REVOKED'}
+
+    def is_keybox_revoked(self, xml: Element) -> bool:
+        for cert_pem in xml.findall('.//Key[@algorithm="ecdsa"]/CertificateChain/Certificate'):
+            hex_serial, issuer_serial = get_cert_info(cert_pem)
+            found = (hex_serial and hex_serial in self.revoked) or (issuer_serial and issuer_serial in self.revoked)
+
+            if found:
+                return False
+
+        return True
+
