3b7565b206bb57071cfaaa5fee34106074473dfe
Eric Siegel <NTICompass@gmail.com>
2025-09-18 08:25:34 -0400

    use `load_pem_x509_certificates` to load all certs at once

--- verify/googlecheck.py
+++ verify/googlecheck.py
@@ -1,21 +1,20 @@
 from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 from time import time
-from typing import Optional
+from typing import Generator, Optional
 from xml.etree.ElementTree import Element
 import requests
 
 
-def get_cert_info(cert_pem: Element) -> tuple[Optional[str], Optional[str]]:
+def get_certs_info(certs: list[bytes]) -> Generator[tuple[Optional[str], Optional[str]]]:
     try:
-        cert = x509.load_pem_x509_certificate(cert_pem.text.encode(), default_backend())
+        for cert in x509.load_pem_x509_certificates(b''.join(certs)):
+            hex_serial = format(cert.serial_number, 'x').lower().lstrip('0')
+            issuer_serial = {attr.value.lower() for attr in cert.issuer if attr.oid == x509.NameOID.SERIAL_NUMBER}
+
+            yield hex_serial, str(issuer_serial.pop()) if len(issuer_serial) > 0 else None
     except ValueError:
-        return None, None
-
-    hex_serial = format(cert.serial_number, 'x').lower().lstrip('0')
-    issuer_serial = {attr.value.lower() for attr in cert.issuer if attr.oid == x509.NameOID.SERIAL_NUMBER}
-
-    return hex_serial, str(issuer_serial.pop()) if len(issuer_serial) > 0 else None
+        for x in range(len(certs)):
+            yield None, None
 
 """
 Thanks to https://github.com/Ante0/YetAnotherKeyboxChecker
@@ -32,8 +31,9 @@ class GoogleChecker:
         self.revoked: set[str] = {key for (key, status) in keybox_status['entries'].items() if status['status'] == 'REVOKED'}
 
     def is_keybox_valid(self, xml: Element) -> bool:
-        for cert_pem in xml.findall('.//Key[@algorithm="ecdsa"]/CertificateChain/Certificate'):
-            hex_serial, issuer_serial = get_cert_info(cert_pem)
+        xml_certs = xml.findall('.//Key[@algorithm="ecdsa"]/CertificateChain/Certificate')
+
+        for (hex_serial, issuer_serial) in get_certs_info([cert_pem.text.encode() for cert_pem in xml_certs]):
             found = (hex_serial and hex_serial in self.revoked) or (issuer_serial and issuer_serial in self.revoked)
 
             if found:
