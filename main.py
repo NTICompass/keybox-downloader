e70a9a2c472dcea65e3a5fe018339334f4259f33
Eric Siegel <NTICompass@gmail.com>
2025-09-19 08:17:39 -0400

    backup existing keyboxes before downloading new ones

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from downloaders.integritybox import IntegrityBox
 from downloaders.trickyaddon import TrickyAddon
 from downloaders.tsupport import TSupport
 from downloaders.yurikey import YuriKey
+from shutil import make_archive, rmtree
 from time import time
 from tqdm import tqdm
 from types import GeneratorType
@@ -11,22 +12,35 @@ from xml.etree.ElementTree import ElementTree
 import logging
 import os
 
+path = 'keyboxes'
+types = ('revoked', 'valid')
+log_path = 'logs'
+backup_path = 'backups'
 
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    path = 'keyboxes'
-    types = ('valid', 'revoked')
+    def make_folder(folder: str):
+        if not os.path.exists(folder):
+            os.mkdir(folder)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-
-    if not os.path.exists(path):
+    def make_folders():
         for key_type in types:
             os.makedirs(f'{path}/{key_type}')
 
-    logging.basicConfig(filename=f'logs/keybox-downloader-{time():.0f}.log', level=logging.INFO)
+    make_folder(log_path)
+    logging.basicConfig(filename=f'{log_path}/keybox-downloader-{time():.0f}.log', level=logging.INFO)
     logger.info('Starting Keybox Downloader')
+
+    if not os.path.exists(path):
+        make_folders()
+    else:
+        logger.info('Backing up existing keyboxes')
+
+        make_folder(backup_path)
+        make_archive(f'{backup_path}/keyboxes-{time():.0f}', 'zip', path)
+        rmtree(path)
+        make_folders()
 
     checker = GoogleChecker()
 
@@ -41,7 +55,7 @@ if __name__ == '__main__':
 
             logger.info(f'Checking keybox #{keybox_idx:d}' if is_generator else 'Checking keybox')
             valid_keybox = checker.is_keybox_valid(keybox_file)
-            save_path = f'{path}/{'valid' if valid_keybox else 'revoked'}'
+            save_path = f'{path}/{types[int(valid_keybox)]}'
             file_name = f'{save_path}/{type(dl).__name__ + (f'_{keybox_idx:d}' if is_generator else '')}.xml'
 
             logger.info(f'Saving keybox #{keybox_idx:d}' if is_generator else 'Saving keybox')
