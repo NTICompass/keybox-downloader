239c8556c5d0458a75ac47055fe73fdb3583ea0c
Eric Siegel <NTICompass@gmail.com>
2025-09-30 15:44:07 -0400

    update google key checker check all certs in keybox

--- utils/googlecheck.py
+++ utils/googlecheck.py
@@ -28,13 +28,13 @@ class GoogleChecker:
                 'Cache-Control': 'no-cache',
             })).json()
 
-        self.revoked = {key for key, status in self.status_list['entries'].items() if status['status'] == 'REVOKED'}
+            self.revoked = {key for key, status in self.status_list['entries'].items() if status['status'] == 'REVOKED'}
 
         ec_certs = xml.findall('.//Key[@algorithm="ecdsa"]/CertificateChain/Certificate')
         rsa_certs = xml.findall('.//Key[@algorithm="rsa"]/CertificateChain/Certificate')
         self.logger.info(f'Found {len(ec_certs)} EC and {len(rsa_certs)} RSA certs')
 
-        for hex_serial, issuer_serial in self.get_certs_info([ec_certs[0].text.encode(), rsa_certs[0].text.encode()]):
+        for hex_serial, issuer_serial in self.get_certs_info(*(cert.text.encode() for cert in ec_certs), *(cert.text.encode() for cert in rsa_certs)):
             found = (hex_serial and hex_serial in self.revoked) or (issuer_serial and issuer_serial in self.revoked)
             self.logger.info('Cert is revoked' if found else 'Cert is valid')
 
@@ -43,12 +43,11 @@ class GoogleChecker:
 
         return True
 
-    def get_certs_info(self, certs: list[bytes]) -> Generator[tuple[Optional[str], Optional[str]]]:
+    def get_certs_info(self, *certs: bytes) -> Generator[tuple[Optional[str], Optional[str]]]:
         try:
             self.logger.info(f'Loading {len(certs)} certs')
 
-            # Load each cert individually, since `load_pem_x509_certificates` will crash if ANY are invalid
-            for cert in (x509.load_pem_x509_certificate(cert_pem) for cert_pem in certs):
+            for cert in x509.load_pem_x509_certificates(b''.join(certs)):
                 hex_serial = f'{cert.serial_number:x}'
                 issuer_serial = {attr.value.lower() for attr in cert.issuer if attr.oid == x509.NameOID.SERIAL_NUMBER}
 
