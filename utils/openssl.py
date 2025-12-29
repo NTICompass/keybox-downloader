31971ebe85f2a6c4d40a9b54f320edd1e9f5e0c9
Eric Siegel <NTICompass@gmail.com>
2025-12-29 09:27:47 -0500

    implement our version of `load_pem_x509_certificates`

--- utils/openssl.py
+++ utils/openssl.py
@@ -1,12 +1,18 @@
+import re
+
 """
 If we don't have the `cryptography` module, then just call `openssl` from the command line
 """
 class x509:
+    BEGIN = '-----BEGIN CERTIFICATE-----'
+    END = '-----END CERTIFICATE-----'
+
     def load_pem_x509_certificates(self, data: bytes) -> list[x509_cert]:
-        pass
+        return [x509_cert(cert) for cert in re.findall(rf'{self.BEGIN}(.+?){self.END}', data.decode('ascii'))]
 
 """
 To get the serial, use `openssl x509 -inform pem -noout -serial`
 """
 class x509_cert:
-    pass
+    def __init__(self, cert: str):
+        pass
