aa3611f21ed1374e2f2529f7739a03cc5ce27ebc
Eric Siegel <NTICompass@gmail.com>
2026-01-05 12:11:55 -0500

    yurikey removed the `conf` file, now they just use a `key` file

--- downloaders/yurikey.py
+++ downloaders/yurikey.py
@@ -8,51 +8,14 @@ import xml.etree.ElementTree as ET
 
 class YuriKey(Downloader):
     # https://t.me/s/yuriiroot
-    URL = 'https://github.com/Yurii0307/yurikey/raw/refs/heads/main/conf'
-    # I don't know if this is used by the module or the key inside the `conf`
-    KEY_URL = 'https://github.com/Yurii0307/yurikey/raw/refs/heads/main/key'
+    URL = 'https://github.com/Yurii0307/yurikey/raw/refs/heads/main/key'
 
-    async def get_keybox(self) -> AsyncGenerator[Element]:
-        self.logger.info('Decoding keybox xml')
-        keyboxes = []
-
-        # Get the `conf` keybox
-        self.encoded = await self.get_encoded_keybox()
-        keyboxes.append(self.decode_keybox())
-
-        # And the `key` file
-        self.encoded = (await self.client.get(self.KEY_URL)).text
-        keyboxes.append(self.decode_keybox())
-
-        # Get the keybox from the `conf` file as well as the `key` file
-        for idx, keybox in enumerate(keyboxes):
-            xml = fix_rsa_keys(ET.fromstring(keybox))
-
-            keybox_id = xml.find('.//Keybox[@DeviceID]')
-            keybox_id.set('DeviceID', f'{keybox_id.get("DeviceID")} {idx + 1:d}')
-
-            yield xml
-
-    async def get_encoded_keybox(self) -> str:
+    async def get_keybox(self) -> Element:
         self.logger.info('Downloading encoded keybox')
+        self.encoded = await anext(self.download_urls())
 
-        keybox_script = b64decode(await anext(self.download_urls())).decode('utf-8')
-        keybox_vars = get_var_from_shell(keybox_script, ['KEYBOX_BASE64_PAYLOAD'])
-        return keybox_vars['KEYBOX_BASE64_PAYLOAD']
+        return ET.fromstring(self.decode_keybox())
+
 
     def decode_keybox(self) -> str:
-        encoded = self.encoded
-
-        loops = 0
-        max_loops = 10
-
-        # Decode base64 until it's an XML file (the `conf` file had it dual-encoded)
-        while True:
-            encoded = b64decode(encoded).decode('ascii')
-
-            if encoded.strip().startswith('<?xml'):
-                break
-            elif ++loops == max_loops:
-                raise ValueError('Cannot decode keybox')
-
-        return encoded
+        return b64decode(self.encoded).decode('ascii')
