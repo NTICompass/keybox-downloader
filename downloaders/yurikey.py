0df821cda584cf95406072053498f239dc1a1cd0
Eric Siegel <NTICompass@gmail.com>
2026-02-24 08:17:26 -0500

    use the filebin API to download the yurikey xml

--- downloaders/yurikey.py
+++ downloaders/yurikey.py
@@ -1,8 +1,10 @@
-from . import Downloader, fix_rsa_keys
+from . import Downloader
 from base64 import b64decode
+from bs4 import BeautifulSoup
 from collections.abc import AsyncGenerator
-from utils.shellvar import get_var_from_shell
+from io import BytesIO
 from xml.etree.ElementTree import Element, ParseError
+from zipfile import ZipFile
 import xml.etree.ElementTree as ET
 
 
@@ -14,10 +16,27 @@ class YuriKey(Downloader):
         self.logger.info('Downloading encoded keybox')
         self.encoded = await anext(self.download_urls())
 
+        data = self.decode_keybox()
         try:
-            yield ET.fromstring(self.decode_keybox())
+            yield ET.fromstring(data)
         except ParseError:
-            yield None
+            # We might have an HTML file from "filebin.net"
+            # https://filebin.net/api
+            self.logger.info('XML parsing failed, assuming filebin HTML')
+            soup = BeautifulSoup(data, 'html.parser')
+
+            for code in soup.find_all('code'):
+                self.logger.info('Downloading ZIP from filebin.net')
+                url = f'https://filebin.net/archive/{code.get_text()}/zip'
+                zip_data = (
+                    await self.client.get(url, headers={'User-Agent': 'curl/8.18.0'})
+                ).content
+
+                with ZipFile(BytesIO(zip_data), 'r') as zip_file:
+                    self.logger.info('Unzipping data...')
+                    for name in zip_file.namelist():
+                        with zip_file.open(name) as keybox_data:
+                            yield ET.fromstring(keybox_data.read())
 
     def decode_keybox(self) -> str:
         return b64decode(self.encoded).decode('ascii')
