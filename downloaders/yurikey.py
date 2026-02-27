f7e7168f7ecdaaed33428395e9a59f2146fd994f
Eric Siegel <NTICompass@gmail.com>
2026-02-27 10:32:55 -0500

    remove "filebin" code from YuriKei

--- downloaders/yurikey.py
+++ downloaders/yurikey.py
@@ -16,36 +16,7 @@ class YuriKey(Downloader):
         self.logger.info('Downloading encoded keybox')
         self.encoded = await anext(self.download_urls())
 
-        data = self.decode_keybox()
-        try:
-            yield ET.fromstring(data)
-        except ParseError:
-            # We might have an HTML file from "filebin.net"
-            # https://filebin.net/api
-            self.logger.info('XML parsing failed, assuming filebin HTML')
-            soup = BeautifulSoup(data, 'html.parser')
-
-            for code in soup.find_all('code'):
-                self.logger.info('Downloading ZIP from filebin.net')
-                zip_dl = await self.client.get(
-                    f'https://filebin.net/archive/{code.get_text()}/zip',
-                    headers={'User-Agent': 'curl/8.18.0'},
-                )
-
-                if zip_dl.status_code == 404:
-                    yield None
-                else:
-                    with ZipFile(BytesIO(zip_dl.content), 'r') as zip_file:
-                        self.logger.info('Unzipping data...')
-                        for name in zip_file.namelist():
-                            with zip_file.open(name) as keybox_data:
-                                yield ET.parse(keybox_data).getroot()
-
-                    self.logger.info('Deleting filebin, for the lulz')
-                    await self.client.delete(
-                        f'https://filebin.net/{code.get_text()}',
-                        headers={'User-Agent': 'curl/8.18.0'},
-                    )
+        yield ET.fromstring(self.decode_keybox())
 
     def decode_keybox(self) -> str:
         return b64decode(self.encoded).decode('ascii')
