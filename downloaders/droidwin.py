b2dc6caa680aab533d1bf41f646d6e5f0395b59c
Eric Siegel <NTICompass@gmail.com>
2026-04-23 16:11:44 -0400

    drodwin may have a cloudflare challenge! skip it for now, i'll try to fix it later

--- downloaders/droidwin.py
+++ downloaders/droidwin.py
@@ -16,26 +16,29 @@ class DroidWin(Downloader):
     async def get_keybox(self) -> AsyncGenerator[Element | None]:
         self.logger.info('Downloading webpage')
 
-        soup = BeautifulSoup(await anext(self.download_urls()), 'html.parser')
-        links = soup.find_all(
-            'a',
-            {
-                'href': re.compile(
-                    r'^https://droidwin.com/wp-content/.+/droidwin-keybox-.+\.zip'
-                )
-            },
-        )
-
-        for link in links:
-            self.logger.info('Downloading module ZIP file')
-            zip_dl = await self.client.get(
-                link.attrs['href'], headers={'User-Agent': 'curl/8.18.0'}
+        try:
+            soup = BeautifulSoup(await anext(self.download_urls()), 'html.parser')
+            links = soup.find_all(
+                'a',
+                {
+                    'href': re.compile(
+                        r'^https://droidwin.com/wp-content/.+/droidwin-keybox-.+\.zip'
+                    )
+                },
             )
 
-            with ZipFile(BytesIO(zip_dl.content), 'r') as zip_file:
-                self.logger.info('Extracting keybox from ZIP file')
-                with zip_file.open('keybox.xml') as keybox_data:
-                    yield ET.parse(keybox_data).getroot()
+            for link in links:
+                self.logger.info('Downloading module ZIP file')
+                zip_dl = await self.client.get(
+                    link.attrs['href'], headers={'User-Agent': 'curl/8.18.0'}
+                )
+
+                with ZipFile(BytesIO(zip_dl.content), 'r') as zip_file:
+                    self.logger.info('Extracting keybox from ZIP file')
+                    with zip_file.open('keybox.xml') as keybox_data:
+                        yield ET.parse(keybox_data).getroot()
+        except StopAsyncIteration:
+            yield None
 
     def decode_keybox(self) -> str:
         raise NotImplementedError('Keybox not encoded')
