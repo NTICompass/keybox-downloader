a04d94dac262cfc3811613fef9a68426e26e2373
Eric Siegel <NTICompass@gmail.com>
2026-05-09 00:18:45 -0400

    make sure `__main__.root` is set before loading other files

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from pathlib import Path
+import sys
+
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    # Inside a PyInstaller bundle
+    root = Path(sys._MEIPASS)
+else:
+    root = Path(__file__).parent
+
 from program import get_downloaders, go, menu
 import argparse
 import asyncio
-import sys
 
 if __name__ == '__main__':
-    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
-        # Inside a PyInstaller bundle
-        root = Path(sys._MEIPASS)
-    else:
-        root = Path(__file__).parent
-        
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--download', dest='download', action='store_true')
     parser.add_argument('-i', '--install', dest='install', action='store_true')
