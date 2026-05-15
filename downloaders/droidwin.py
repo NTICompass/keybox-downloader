7a952db77a1ae5978a1a3f54166eabb6881a255a
Eric Siegel <NTICompass@gmail.com>
2026-05-15 13:10:09 -0400

    in `DroidWin`, it could download the webpage but fail (403) on the zip download

--- downloaders/droidwin.py
+++ downloaders/droidwin.py
@@ -1,6 +1,6 @@
 from . import Downloader
 from bs4 import BeautifulSoup
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncGenerator, Callable
 from program.keybox import Keybox
 from typing import final, override
 import re
@@ -13,24 +13,22 @@ class DroidWin(Downloader):
     )
 
     @override
+    def __init__(self):
+        super().__init__()
+        self.cloudflare = False
+
+    @override
     async def process(
         self, downloaded: AsyncGenerator[str]
     ) -> AsyncGenerator[Keybox | None]:
         self.logger.info('Downloading webpage')
-
-        cloudflare = False
-
-        try:
-            html = await anext(downloaded)
-        except StopAsyncIteration:
-            """
-            This means that the httpx returned an error downloading the website
-            Probably a 403, probably a CloudFlare challenge...
-            """
-            self.logger.info('Hit CloudFlare challenge, trying to solve it')
-
-            cloudflare = True
-            html = await anext(self.download_urls(cloudflare=True))
+        html = await self.do_download(
+            lambda: (
+                downloaded
+                if not self.cloudflare
+                else self.download_urls(cloudflare=True)
+            )
+        )
 
         soup = BeautifulSoup(html, 'html.parser')
         links = soup.find_all(
@@ -44,15 +42,32 @@ class DroidWin(Downloader):
 
         for link in links:
             self.logger.info('Downloading module ZIP file')
-            zip_dl = await anext(
-                self.download_urls(
+            zip_dl = await self.do_download(
+                lambda: self.download_urls(
                     binary=True,
-                    cloudflare=cloudflare,
+                    cloudflare=self.cloudflare,
                     download=(str(link.attrs['href']),),
                 )
             )
 
             yield self.unzip(zip_dl, 'keybox.xml')
+
+    async def do_download[T: str | bytes](
+        self, dl: Callable[[], AsyncGenerator[T]]
+    ) -> T:
+        try:
+            data = await anext(dl())
+        except StopAsyncIteration:
+            """
+            This means that the httpx returned an error downloading the website
+            Probably a 403, probably a CloudFlare challenge...
+            """
+            self.logger.info('Hit CloudFlare challenge, trying to solve it')
+
+            self.cloudflare = True
+            data = await anext(dl())
+
+        return data
 
     @override
     def decode(self, encoded: str) -> str:
