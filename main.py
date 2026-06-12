0feb846fd8a05ffd50ddb6d1f4b2498e3603a9e2
Eric Siegel <NTICompass@gmail.com>
2026-06-12 10:48:15 -0400

    fix nuitka paths (root vs exe_root)

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from pathlib import Path
 import sys
 
-root = (
-    Path(sys._MEIPASS) if hasattr(sys, '_MEIPASS') else Path(__file__).resolve().parent
+# https://nuitka.net/user-documentation/common-issue-solutions.html#onefile-finding-files
+is_nuitka = '__compiled__' in globals()
+
+# https://pyinstaller.org/en/stable/runtime-information.html
+is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+
+# `root` is where "internal files" are, like the "scripts" directory
+root = Path(sys._MEIPASS) if is_pyinstaller else Path(__file__).resolve().parent
+
+# `exe_root` is where the program was run from, where "keyboxes" and such go
+exe_root = (
+    Path(sys.executable).parent
+    if is_pyinstaller
+    else (Path(sys.argv[0]).parent if is_nuitka else root)
 )
-exe_root = Path(sys.executable).parent if getattr(sys, 'frozen', False) else root
 
 # ruff: disable[E402]
 from program.action import get_downloaders, go
