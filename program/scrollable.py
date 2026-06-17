709582fd7d7421c82e50d1f52a4daf8136c3f5f6
Eric Siegel <NTICompass@gmail.com>
2026-06-17 10:42:47 -0400

    more code cleanup

--- program/scrollable.py
+++ program/scrollable.py
@@ -1,7 +1,8 @@
+from asyncio import Task
 from collections.abc import Callable
 from prompt_toolkit.layout.controls import FormattedTextControl
 from prompt_toolkit.mouse_events import MouseEventType, MouseEvent
-from typing import Awaitable, override, final
+from typing import final, override
 
 
 @final
@@ -9,7 +10,7 @@ class ScrollableTextControl(FormattedTextControl):
     def __init__(
         self,
         *args,
-        on_scroll: Callable[[int], None | Awaitable[None]] = lambda x: None,
+        on_scroll: Callable[[int], None | Task[None]] = lambda _: None,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
@@ -17,11 +18,13 @@ class ScrollableTextControl(FormattedTextControl):
 
     @override
     def mouse_handler(self, mouse_event: MouseEvent):
-        if mouse_event.event_type == MouseEventType.SCROLL_UP:
-            self.__on_scroll(-1)
-            return None
-        elif mouse_event.event_type == MouseEventType.SCROLL_DOWN:
-            self.__on_scroll(1)
+        delta = {
+            MouseEventType.SCROLL_UP: -1,
+            MouseEventType.SCROLL_DOWN: 1,
+        }.get(mouse_event.event_type)
+
+        if delta is not None:
+            self.__on_scroll(delta)
             return None
 
         # Let FormattedTextControl dispatch clicks to fragment handlers.
