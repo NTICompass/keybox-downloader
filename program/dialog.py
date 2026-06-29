95138cf942912333ef46b55c8cd16bd71736584c
Eric Siegel <eric@reallycool.email>
2026-06-29 15:09:13 -0400

    update `AwaitableDialog`

--- program/dialog.py
+++ program/dialog.py
@@ -11,10 +11,11 @@ class AwaitableDialog[T: str](Dialog):
     @override
     def __init__(self, buttons: Sequence[Button] | None = None, **kwargs):
         self._future = get_running_loop().create_future()
-        buttons = list(buttons or [])
 
-        buttons.append(Button(text='Ok', handler=lambda: self.finish(None)))
-        super().__init__(buttons=buttons, **kwargs)
+        dialog_buttons: list[Button] = list(buttons or [])
+        dialog_buttons.append(Button(text='Ok', handler=lambda: self.finish(None)))
+
+        super().__init__(buttons=dialog_buttons, **kwargs)
 
     def finish(self, result: T | None) -> None:
         if not self._future.done():
