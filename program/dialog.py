60e6c878a6e3df92fea9bb6156e8594d2da55590
Eric Siegel <eric@reallycool.email>
2026-07-24 09:54:01 -0400

    clean up `AwaitableDialog`

--- program/dialog.py
+++ program/dialog.py
@@ -1,16 +1,25 @@
+# SPDX-FileCopyrightText: Copyright 2026 gen\Eric Computers
+# SPDX-License-Identifier: MIT
+
+"""An `AwaitableDialog` that can be used in `prompt_toolkit."""
+
 from asyncio import Future, get_running_loop
-from collections.abc import Generator, Sequence
-from typing import Any, final, override
+from typing import TYPE_CHECKING, Any, final, override
 
 from prompt_toolkit.widgets import Button, Dialog
+
+if TYPE_CHECKING:
+    from collections.abc import Generator, Sequence
 
 
 @final
 class AwaitableDialog[T: str](Dialog):
+    """A dialog you can `await` (resolves when the 'ok' button is pressed)."""
+
     _future: Future[T | None]
 
     @override
-    def __init__(self, buttons: Sequence[Button] | None = None, **kwargs):
+    def __init__(self, buttons: Sequence[Button] | None = None, **kwargs: Any) -> None:
         self._future = get_running_loop().create_future()
 
         dialog_buttons: list[Button] = list(buttons or [])
@@ -19,8 +28,15 @@ class AwaitableDialog[T: str](Dialog):
         super().__init__(buttons=dialog_buttons, **kwargs)
 
     def finish(self, result: T | None) -> None:
+        """Resolve the dialog's `Future`."""
         if not self._future.done():
             self._future.set_result(result)
 
     def __await__(self) -> Generator[Any, None, T | None]:
+        """Lets you just `await dialog` instead of `await dialog.future`.
+
+        Returns:
+            The value set by the `finish()` method
+
+        """
         return self._future.__await__()
